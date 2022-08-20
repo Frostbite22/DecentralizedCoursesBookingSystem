ff890924cfe26d733dd01ac48d77cbd938e9028b
Frostbite22 <landoulsiferes@outlook.com>
2022-08-20 12:21:22 +0100

    level class

--- contracts/Level.sol
+++ contracts/Level.sol
@@ -5,6 +5,39 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract Level 
-{
+{   
+
+    uint16 private levelId ; 
+    string private description ; 
+    string private imageUrl ; 
+
+    constructor(uint16 _levelId)
+    {
+        levelId = _levelId ; 
+    }
+
+    function setDescription(string memory _description) public 
+    {
+        description = _description ; 
+    }
+
+    function getDescription() view public returns(string memory)
+    {
+        return description ; 
+    }
+
+    function setImageUrl(string memory _imageUrl) public 
+    {
+        imageUrl = _imageUrl ; 
+    }
+
+    function getImageUrl() view public returns(string memory)
+    {
+        return imageUrl ; 
+    }
     
+    function getLevelId() view public returns(uint16)
+    {
+        return levelId ; 
+    }
 } 
